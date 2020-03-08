c44d080b364dff85f781114a4e026ad716a39e7b
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-03-08 10:06:56 +0100

    initial commit

---
+++ src/test/kotlin/org/integrational/kotlin/KotlinSpringBootRestServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.integrational.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootRestServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
