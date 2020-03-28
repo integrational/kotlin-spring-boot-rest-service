8d9c13618c265c2ddd40e2beef72d042ebc2bccd
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-03-28 23:57:24 +0100

    added Spring Boot Actuator and using Jetty

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     val kotlinVersion = "1.3.71"
 
-    id("org.springframework.boot") version "2.2.6.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
+
+    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
 }
 
 val javaVersion = JavaVersion.VERSION_11
+val bootGrp = "org.springframework.boot"
 
 group = "org.integrational"
 version = "0.0.1-SNAPSHOT"
@@ -17,15 +19,19 @@ java.sourceCompatibility = javaVersion
 java.targetCompatibility = javaVersion
 
 repositories {
-    mavenCentral()
+    mavenCentral() // for Spring Boot BOM
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(group = bootGrp, name = "spring-boot-starter-web") {
+        exclude(group = bootGrp, module = "spring-boot-starter-tomcat")
+    }
+    implementation(group = bootGrp, name = "spring-boot-starter-jetty")
+    implementation(group = bootGrp, name = "spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+    testImplementation(group = bootGrp, name = "spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
 }
@@ -39,4 +45,8 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = javaVersion.majorVersion
     }
+}
+
+springBoot {
+    buildInfo() // generate META-INF/build-info.properties for Spring Boot Actuator
 }
