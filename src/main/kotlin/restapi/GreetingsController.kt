a384a9e787047d90e000cf2a3e81c0e733406567
GeraldLoeffler <gerald.loeffler@googlemail.com>
2020-03-29 12:24:19 +0200

    removed superfluous @Inject constructor annotations

--- src/main/kotlin/restapi/GreetingsController.kt
+++ src/main/kotlin/restapi/GreetingsController.kt
@@ -2,10 +2,9 @@ package org.integrational.greetings.restapi
 
 import org.integrational.greetings.domain.service.GreetingsService
 import org.springframework.web.bind.annotation.RestController
-import javax.inject.Inject
 
 @RestController
-class GreetingsController(@Inject private val svc: GreetingsService) : GreetingsAPI {
+class GreetingsController(private val svc: GreetingsService) : GreetingsAPI {
     override fun add(toAdd: GreetingToAdd) = svc.add(toAdd.name)
     override fun getByName(name: String) = svc.get(name)
     override fun getAll() = svc.getAll()
